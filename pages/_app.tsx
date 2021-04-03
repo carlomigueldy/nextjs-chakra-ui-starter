a663a534afc7801d180624501098d323a764299b
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-04-03 20:41:19 +0800

    feat(font-family): installed `Sora` as primary font-family for `body` and `heading`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
 import { AppProps } from "next/dist/next-server/lib/router/router";
-import "../styles/globals.css";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import "../styles/globals.css";
+import "@fontsource/sora";
+
+const theme = extendTheme({
+  fonts: {
+    body: "Sora",
+    heading: "Sora",
+  },
+});
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <ChakraProvider theme={theme}>
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+  );
 }
 
-export default MyApp;
+export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,69 @@
+import {
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+} from "@chakra-ui/modal";
+import { Box, Button, Heading, Input, useDisclosure } from "@chakra-ui/react";
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import React from "react";
+// import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef();
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-    </div>
+    <>
+      <main
+        style={{
+          height: "100vh",
+          display: "flex",
+        }}
+      >
+        <Box
+          height="100%"
+          width="300px"
+          bgColor="white"
+          padding="5"
+          position="fixed"
+          outline="solid 1px"
+          outlineColor="gray.300"
+        >
+          <Heading>Hello World</Heading>
+
+          <Button ref={btnRef} colorScheme="teal" onClick={onOpen}>
+            Open
+          </Button>
+          <Drawer
+            isOpen={isOpen}
+            placement="right"
+            onClose={onClose}
+            finalFocusRef={btnRef}
+          >
+            <DrawerOverlay>
+              <DrawerContent>
+                <DrawerCloseButton />
+                <DrawerHeader>Create your account</DrawerHeader>
+
+                <DrawerBody>
+                  <Input placeholder="Type here..." />
+                </DrawerBody>
+
+                <DrawerFooter>
+                  <Button variant="outline" mr={3} onClick={onClose}>
+                    Cancel
+                  </Button>
+                  <Button colorScheme="blue">Save</Button>
+                </DrawerFooter>
+              </DrawerContent>
+            </DrawerOverlay>
+          </Drawer>
+        </Box>
+      </main>
+    </>
   );
 }
